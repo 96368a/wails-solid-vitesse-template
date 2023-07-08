0ef58d1d6c612f22bf677161dadb1c979aaee670
96368a <ncxxg@qq.com>
2023-07-09 03:10:46 +0800

    :tada: Initialize

---
+++ frontend/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="vitest" />
+
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import Pages from 'vite-plugin-pages'
+import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    solidPlugin(),
+    Pages(),
+    Unocss(),
+    AutoImport({
+      imports: [
+        'solid-js',
+        '@solidjs/router',
+      ],
+      dts: true,
+      dirs: [
+        'src/primitives',
+      ],
+    }),
+
+  ],
+  build: {
+    target: 'esnext',
+  },
+  test: {
+    environment: 'jsdom',
+    transformMode: {
+      web: [/.[jt]sx?/],
+    },
+    threads: false,
+    isolate: false,
+  },
+})
